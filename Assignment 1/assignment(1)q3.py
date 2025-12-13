3f8493461e133b9ba4135f12150a8f45f13934a7
atharvbagade-7 <atharvbagade27@gmail.com>
2025-12-13 08:47:24 +0530

    Add files via upload

---
+++ Assignment 1/assignment(1)q3.py
@@ -0,0 +1,12 @@
+a = int(input("Enter 1st no:"))
+b = int(input("Enter 2nd no:"))
+
+print("\nBefore swapping:")
+print("a=",a)
+print("b=",b)
+
+
+a, b=b ,a
+print("\nAfter swapping:")
+print("a=",a)
+print("b=",b)
---
+++ Assignment 1/assignment(1)q4.py
@@ -0,0 +1,7 @@
+a = float(input("Enter first no:"))
+b = float(input("Enter 2nd no:"))
+
+print("Addition=",a+b)
+print("Substraction=",a-b)
+print("Multiplication=",a*b)
+print("Divison=",a/b)
